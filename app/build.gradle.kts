5f7fb78bef5b9805e080b30795dc0de031f9eabd
root <root@SRITESH-SURANJAN.>
2025-05-12 17:19:46 +0000

    files commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 	alias(libs.plugins.kotlin.compose)
 	alias(libs.plugins.kotlin.serialization)
 	id("com.google.dagger.hilt.android")
-	id("com.google.devtools.ksp") version "2.0.21"
+	id("com.google.devtools.ksp")
 }
 
 android {
